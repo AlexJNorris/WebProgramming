f43db5745bfe9fc0c4376e7cc0ce14154d5b2d8d
AlexJNorris <a.norris97@icloud.com>
2021-09-19 18:28:58 -0500

    Add completed ICP 4

---
+++ Web_ICP4/Source/GithubAPIUserData/gitapi.js
@@ -0,0 +1,33 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+const writeUserData = (user) => {
+    const pfp = document.getElementById("pfp");
+    const name = document.getElementById("name");
+    const uname = document.getElementById("userName");
+    const link = document.getElementById("link");
+    pfp.src = user.data.avatar_url;
+    name.textContent = "Name:    " + user.data.name;
+    uname.textContent = "Username:    " + user.data.login;
+    link.href = user.data.html_url;
+    link.textContent = "GitHub Link";
+}
+
+async function getUser(userName) {
+    const octokit = new Octokit();
+    const result = await octokit.request('GET /users/' + userName);
+
+    console.log(`${result.data.avatar_url}`);
+    writeUserData(result);
+}
+
+const listener = () => {
+    const input = document.getElementById("in");
+    //listen for enter to send input value
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            getUser(input.value)
+        }
+    });
+}
+
+listener();
---
+++ Web_ICP4/Source/ImageHover/hover_script.js
@@ -0,0 +1,14 @@
+
+
+function upDate(previewPic){
+    const imageDiv = document.querySelector('#image');
+    imageDiv.style.backgroundImage = "url('"+previewPic.src + "')";
+    imageDiv.textContent = previewPic.alt;
+}
+
+function unDo(){
+
+    const imageDiv = document.querySelector('#image');
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.textContent = "Hover over an image below to display here.";
+}
